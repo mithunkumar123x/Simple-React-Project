c0a76ea0063cb1574f80b5e31c2bb8064f0ab692
mithunkumar123x <mithunkumar123x@gmail.com>
2024-02-06 20:26:07 +0530

    add and manage list of users via state

--- 01reactproject/src/components/Users/AddUser.js
+++ 01reactproject/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react' ;
+import React, {useState} from 'react' ;
 
 import Card from '../UI/Card'
 
@@ -6,22 +6,49 @@ import classes from './AddUser.module.css';
 
 import Button  from '../UI/Button';
 
+
+
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername ]=useState('')
+    const [enteredAge, setEnteredAge ]=useState('')
 
     const adduserHandler = (event) => {
         event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+          return;
+        }
+        if (+enteredAge < 1) {
+          return;
+        }
+        props.onAddUser(enteredUsername,enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
+    const usernameChangeHandler = (event) => {
+      setEnteredUsername(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+      setEnteredAge(event.target.value);
+    }
     return (
      <Card className = {classes.input}>
       <form onSubmit={adduserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text'/>
+        <input id='username'
+         type='text' 
+         value={enteredUsername}
+         onChange={usernameChangeHandler}
+         />
         <label htmlFor='age'>Age (Years)</label>
-        <input  id='age' type='number' />
+        <input  id='age'
+         type='number'
+         value={enteredAge}
+         onChange={ageChangeHandler} 
+         />
         <Button type='submit'>Add Users</Button>
       </form>
-
+    
     </Card>
     );
 };
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Simple-React-Project
 Doing some simple react project here...
+
+
+create project step:
+
+Adding a re-usable  User,Card, Button .
+
+Managing the User Input State.
+
+Adding Validation & Resetting Logic.
+
+Managing a List of Users via State.
+
+Adding The Error Modal component.
+
+Managing the Error State.
+
